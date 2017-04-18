3c53fea695a6867732740904966cf95da2bc7c02
Jirpinya <saii16320@gmail.com>
2017-04-18 21:29:28 +0700

    เป็น OOP แล้ว

--- Sai_servlet-JSP/PaymentServlet.java
+++ Sai_servlet-JSP/PaymentServlet.java
@@ -20,6 +20,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import model.Member;
+import model.Order;
+import model.Payment;
 
 /**
  *
@@ -37,13 +40,6 @@ public class PaymentServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    private Connection conn;
-
-    @Override
-    public void init() {
-        conn = (Connection) getServletContext().getAttribute("connection");
-    }
-
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -56,43 +52,32 @@ public class PaymentServlet extends HttpServlet {
             int NumOfTicket = (int) session.getAttribute("NumOfTicket");
             //String Order_ID = "RD005";
             //String User_ID = "MEM001";
-                        
+
             String SelectBTN = (String) request.getParameter("btn");
             String Payment_method = (String) request.getParameter("method");  //Credit Card,M Cash, Payment
             String Tickets_refund = (String) request.getParameter("refund");
 
             try {
-                Statement stmt = conn.createStatement();
+                
                 float Service_charge = 0;
+                Order order = new Order();
 
-                //---------------CANCLE---------------//
+                //-------------------------CANCLE-------------------------//
                 if (SelectBTN.equals("Cancel")) {
-                    out.println("cancle");
-                    //DELETE Ticket
-                    String sqlc = "Delete From Ticket Where Order_ID = '" + Order_ID + "'";
-                    stmt.executeUpdate(sqlc);
-
                     //DELETE Reservation
-                    String sql1 = "Delete From Reservation Where Order_ID = '" + Order_ID + "'";
-                    stmt.executeUpdate(sql1);
+                    order.deleteOrder(Order_ID);
 
                     RequestDispatcher obj = request.getRequestDispatcher("PurchaseOrder.html");
                     obj.forward(request, response);
                 }
 
-                //---------------CONFIRM--------------//
+                //------------------------CONFIRM-------------------------//
+                //Get Balance from MemberTable
+                Member member = new Member();
+                float balanceBefore = member.getBalance(User_ID);
 
-                //Balance จากตาราง Member
-                String sql_ = "Select Balance from Member where User_ID = '" + User_ID + "'";
-                ResultSet rs = stmt.executeQuery(sql_);
-                rs.next();
-                float price = rs.getFloat("Balance");
-
-                //Total_Price จาก Reservation
-                String sql = "Select Total_Price from Reservation where Order_ID = '" + Order_ID + "'";
-                ResultSet rs1 = stmt.executeQuery(sql);
-                rs1.next();
-                float Total_Price = rs1.getFloat("Total_Price");
+                //Get Total_Price จาก Reservation
+                float Total_Price = order.getTotalPrice(Order_ID);
 
                 if (SelectBTN.equals("Confirm")) {
 
@@ -106,48 +91,36 @@ public class PaymentServlet extends HttpServlet {
 
                     //TICKET REFUND
                     if (Tickets_refund.equals("YES")) {
-                        String sql5 = "Update Reservation SET getRefund = '" + "YES" + "' WHERE Order_ID = '" + Order_ID + "'";
-                        stmt.executeUpdate(sql5);
-
-                        Service_charge += (float) 100*NumOfTicket;
+                        order.setUseTicketRefund(Order_ID);
+                        Service_charge += (float) 100 * NumOfTicket;
                     }
 
                     //UPDATE Service_charge
                     Service_charge = ((Total_Price + Service_charge) * (7 / 100)) + Service_charge;
-                    String sql4 = "Update Reservation SET Service_charge = '" + Service_charge + "' WHERE Order_ID = '" + Order_ID + "'";
-                    stmt.executeUpdate(sql4);
+                    order.setServiceCharge(Service_charge, Order_ID);
 
                     if (Payment_method.equals("mcash")) {
                         //UPDATE Balance
-                        float Balance = price - (Total_Price + Service_charge);
-                        String sql2 = "Update Member SET Balance = '" + Balance + "' WHERE User_ID = '" + User_ID + "'";
-                        stmt.executeUpdate(sql2);
-                        
+                        float Balance = balanceBefore - (Total_Price + Service_charge);
+                        member.UpdateBalance(Balance, User_ID);
+
                         //UPDATE Status
-                        String sqls = "Update Reservation SET Order_Status = '" + "PAID" + "' WHERE Order_ID = '" + Order_ID + "'";
-                        stmt.executeUpdate(sqls);
+                        order.UpdateStatusPAID(Order_ID);
                     }
 
                     //++++++++ Payment Table ++++++++//
                     //Create Payment_ID
-                    String numPay = "Select count(Payment_ID) from Payment where Payment_ID LIKE 'PAY%'";
-                    ResultSet numPay1 = stmt.executeQuery(numPay);
-                    numPay1.next();
-                    String numPayment = numPay1.getString("count(Payment_ID)");
-                    String Payment_ID = "PAY";
-                    for (int i = numPayment.length(); i < 4; i++) {
-                        Payment_ID += "0";
-                    }
-                    Payment_ID += numPayment;
-                    //UPDATE Payment
-                    String sqlp = "Insert into Payment values('" + Order_ID + "', '" + Payment_ID + "', '" + Payment_method + "')";
-                    stmt.executeUpdate(sqlp);
+                    Payment payment = new Payment();
+                    String Payment_ID = payment.getPayment_ID();
 
-                    RequestDispatcher obj = request.getRequestDispatcher("Artical.html");
+                    //UPDATE Payment
+                    payment.insertPayment(Order_ID, Payment_ID, Payment_method);
+
+                    RequestDispatcher obj = request.getRequestDispatcher("index.jsp");
                     obj.forward(request, response);
                 }
             } catch (SQLException ex) {
-                Logger.getLogger(SignUpServlet.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(PaymentServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }

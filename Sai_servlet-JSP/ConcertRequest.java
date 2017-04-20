121227c5c5a7865f369778d77499884195b8f15a
Jirpinya <saii16320@gmail.com>
2017-04-20 19:58:18 +0700

    เป็น OOP แล้ว

--- Sai_servlet-JSP/ConcertRequest.java
+++ Sai_servlet-JSP/ConcertRequest.java
@@ -20,6 +20,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Concert;
 
 /**
  *
@@ -28,13 +29,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ConcertRequest", urlPatterns = {"/ConcertRequest"})
 public class ConcertRequest extends HttpServlet {
 
-    
-     private Connection conn;
-
-    @Override
-    public void init() {
-        conn = (Connection) getServletContext().getAttribute("connection");
-    }
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -48,49 +42,31 @@ public class ConcertRequest extends HttpServlet {
             throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            
-            Statement stmt = conn.createStatement();
-            
+
             ArrayList<String> Concert_Name = new ArrayList<>();
             ArrayList<String> Status = new ArrayList<>();
             ArrayList<Timestamp> Date_Time = new ArrayList<>();
             ArrayList<String> Contact_name = new ArrayList<>();
-            String Concert_table = "SELECT Concert_Name, Status, Date_Time, Fname, Lname "
-                    + "FROM Concert JOIN User USING (User_ID) ORDER BY Date_Time DESC;";
-            ResultSet rs1 = stmt.executeQuery(Concert_table);
-            while(rs1.next()){
-                Concert_Name.add(rs1.getString("Concert_Name"));
-                Status.add(rs1.getString("Status"));
-                Date_Time.add(rs1.getTimestamp("Date_Time"));
-                Contact_name.add(rs1.getString("Fname") + " " + rs1.getString("Lname"));
-                 
-            }
 
-            
+            Concert concert = new Concert();
+            concert.showAllRequest(Concert_Name, Status, Date_Time, Contact_name);
+
             //------ OnlyStatus > ACCEPTED
             ArrayList<String> Concert_NameAc = new ArrayList<>();
             ArrayList<Timestamp> Date_TimeAc = new ArrayList<>();
             ArrayList<String> Contact_nameAc = new ArrayList<>();
-            String ConcertAc_table = "SELECT Concert_Name, Date_Time, Fname, Lname "
-                    + "FROM Concert JOIN User USING (User_ID) WHERE Status = 'ACCEPTED' ORDER BY Date_Time DESC;";
-            ResultSet rs2 = stmt.executeQuery(ConcertAc_table);
-            while(rs2.next()){
-                Concert_NameAc.add(rs2.getString("Concert_Name"));
-                Date_TimeAc.add(rs2.getTimestamp("Date_Time"));
-                Contact_nameAc.add(rs2.getString("Fname") + " " + rs2.getString("Lname"));
-            }
-        
+            concert.AcceptedRequest(Concert_NameAc, Date_TimeAc, Contact_nameAc);
+
             //------ set attribute request
-            request.setAttribute("Concert_Name",Concert_Name);
-            request.setAttribute("Status",Status);
-            request.setAttribute("Contact_name",Contact_name);
-            request.setAttribute("Date_Time",Date_Time);
-            
-            request.setAttribute("Concert_NameAc",Concert_NameAc);
-            request.setAttribute("Contact_nameAc",Contact_nameAc);
-            request.setAttribute("Date_TimeAc",Date_TimeAc);
-            
-            
+            request.setAttribute("Concert_Name", Concert_Name);
+            request.setAttribute("Status", Status);
+            request.setAttribute("Contact_name", Contact_name);
+            request.setAttribute("Date_Time", Date_Time);
+
+            request.setAttribute("Concert_NameAc", Concert_NameAc);
+            request.setAttribute("Contact_nameAc", Contact_nameAc);
+            request.setAttribute("Date_TimeAc", Date_TimeAc);
+
             //----- send to data-concert.jsp
             request.getRequestDispatcher("Allrequest.jsp").forward(request, response);
         }
@@ -108,11 +84,11 @@ public class ConcertRequest extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         try {
-             processRequest(request, response);
-         } catch (SQLException ex) {
-             Logger.getLogger(ConcertRequest.class.getName()).log(Level.SEVERE, null, ex);
-         }
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConcertRequest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -126,11 +102,11 @@ public class ConcertRequest extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         try {
-             processRequest(request, response);
-         } catch (SQLException ex) {
-             Logger.getLogger(ConcertRequest.class.getName()).log(Level.SEVERE, null, ex);
-         }
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConcertRequest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
